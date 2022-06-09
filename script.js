6bab52a7bcb1c224a86aa2aac1c4d13f2926f396
viza <avizaanandya12@gmail.com>
2022-06-09 15:49:25 +0700

    variable added

---
+++ script.js
@@ -0,0 +1,7 @@
+const totalBillInput = document.getElementById('totalBill');
+const btns = document.querySelectorAll('.tip-btns');
+const totalPersonInput = document.getElementById('totalPerson');
+const noPerson = document.querySelector('.no-person');
+const results = document.querySelectorAll('.result');
+const resetBtn = document.querySelector('.reset-btn');
+
