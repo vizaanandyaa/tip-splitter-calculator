dbd2e0b6dd38c1e367227ad86fd22da40ab451d5
viza <avizaanandya12@gmail.com>
2022-06-10 22:39:35 +0700

    set up variable & add eventlistener

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const totalBillInput = document.getElementById('totalBill');
-const btns = document.querySelectorAll('.tip-btns');
+const tipButtons = document.querySelectorAll('.tip-btns');
+const customTipInput = document.getElementById('customTip');
 const totalPersonInput = document.getElementById('totalPerson');
 const noPerson = document.querySelector('.no-person');
 const results = document.querySelectorAll('.result');
-const resetBtn = document.querySelector('.reset-btn');
+const resetButton = document.querySelector('.reset-btn');
+
+//set up "" variable value when refreshed
+totalBillInput.value = "";
+customTipInput.value = "";
+totalPersonInput.value = 1;
+
+//define and setting changing variable 
+let totalBillValue = 0,
+    customTipValue = 0;
+
+//add eventlistener and function
+totalBillInput.addEventListener('input',billInput);
+tipButtons.forEach(tipButton =>{
+    tipButton.addEventListener('click', tipBtn);
+});
+customTipInput.addEventListener('input',tipInput);
+totalPersonInput.addEventListener('input',personInput);
+resetButton.addEventListener('input', resetBtn);
+
+
 
