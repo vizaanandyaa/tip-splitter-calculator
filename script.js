781ef436ae7fd5a9f450c81e79971abfd0cd9940
viza <avizaanandya12@gmail.com>
2022-06-13 19:33:52 +0700

    first done

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const tipButtons = document.querySelectorAll('.tip-btns');
 const customTipInput = document.getElementById('customTip');
 const totalPersonInput = document.getElementById('totalPerson');
 const noPerson = document.querySelector('.no-person');
-const results = document.querySelectorAll('.result');
-const resetButton = document.querySelector('.reset-btn');
+const tipAmountResult = document.querySelector('#tip-amount-result');
+const totalResult = document.querySelector('#total-result');
+const resetButton = document.querySelector('#reset-btn');
 
 //set up "" variable value when refreshed
 totalBillInput.value = "";
@@ -13,7 +14,9 @@ totalPersonInput.value = 1;
 
 //define and setting changing variable 
 let totalBillValue = 0,
-    customTipValue = 0;
+    tipPersonAmount,
+    tipValue = 0,
+    totalPersonValue = 1; 
 
 //add eventlistener and function
 totalBillInput.addEventListener('input',billInput);
@@ -22,12 +25,13 @@ tipButtons.forEach(tipButton =>{
 });
 customTipInput.addEventListener('input',tipInput);
 totalPersonInput.addEventListener('input',personInput);
-resetButton.addEventListener('input', resetBtn);
+resetButton.addEventListener('click', resetBtn);
 
 //function section
 function billInput(){
     totalBillValue = parseFloat(totalBillInput.value);
     calculate();
+
 }
 
 function tipBtn(button){
@@ -35,7 +39,7 @@ function tipBtn(button){
         tipButton.classList.remove('active');
         if(button.target.innerHTML == tipButton.innerHTML){
             tipButton.classList.add('active');
-            totalBillValue = parseFloat(tipButton.innerHTML) / 100;
+            tipValue = parseFloat(tipButton.innerHTML) / 100;
         }
     })
     customTipInput.value = "";
@@ -43,14 +47,52 @@ function tipBtn(button){
 }
 
 function tipInput(){
-    totalBillValue = parseFloat(customTipInput.value) / 100;
+    tipValue = parseFloat(customTipInput.value) / 100;
     tipButtons.forEach(tipButton =>{
         tipButton.classList.remove('active');
     })
     if(customTipInput.value !== ''){
         calculate();
     }
+
 }
 
+function setPersonValue(){
+    if(totalPersonInput.value == "" || totalPersonInput.value == 0){
+        totalPersonInput.style.border = "red 2px solid";
+    }else{
+        totalPersonInput.style.border = "white 2px solid";
+    }
+}
 
+function personInput(){
+    setPersonValue()
+    totalPersonValue = parseFloat(totalPersonInput.value);
+    calculate();
+    
+}
+
+function calculate(){
+    let tipAmount = (tipValue * totalBillValue) / totalPersonValue;
+    tipAmountResult.innerHTML = tipAmount.toFixed();
+
+
+    if(totalPersonInput.value == ""){
+        tipPersonAmount = 0;
+    }else{
+        tipPersonAmount = totalBillValue * (tipValue + 1) / totalPersonValue;
+        totalResult.innerHTML = tipPersonAmount.toFixed();
+    }
+}
+
+function resetBtn(){
+    totalBillInput.value = "";
+    customTipInput.value = "";
+    totalPersonInput.value= 1;
+    tipAmountResult.innerHTML = 0;
+    totalResult.innerHTML = 0;
+    tipButtons.forEach(tipButton =>{
+        tipButton.classList.remove('active');
+    })
+}
 
